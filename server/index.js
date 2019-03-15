81d0eded0f27227536adc08a17a0feeedaa86c8d
gerganajordanova <gerganajordanova68@gmail.com>
2019-03-15 09:52:51 +0200

    'completed'

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 let env = process.env.NODE_ENV || 'development'
 
 let settings = require('./config/settings')[env]
@@ -8,6 +10,15 @@ require('./config/database')(settings)
 require('./config/express')(app)
 require('./config/routes')(app)
 require('./config/passport')()
+
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static('app/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'app', 'build', 'index.html'));
+    });
+  }
 
 app.listen(settings.port)
 console.log(`Server listening on port ${settings.port}...`)
